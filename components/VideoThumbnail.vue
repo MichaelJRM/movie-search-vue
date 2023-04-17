e48631d56f5944886c602d62b5649bafc6ea1b50
Michael <michaeljrmoniz@gmail.com>
2023-04-17 10:41:33 -0300

    VideoThumbnail component.

---
+++ components/VideoThumbnail.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="video_thumbnail rounded-md shadow-md overflow-hidden md:max-w-xs">
+        <div class="flex flex-col">
+            <img class="object-cover" src="https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg" alt="Video Thumbnail">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "VideoThumbnail"
+}
+</script>

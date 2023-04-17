8ceb1f77fa6bb9f84a8d7583e68105066b4cdf4a
Michael <michaeljrmoniz@gmail.com>
2023-04-17 19:26:08 -0300

    More styling and handle edges.

--- components/VideoThumbnail.vue
+++ components/VideoThumbnail.vue
@@ -1,13 +1,36 @@
 <template>
-    <div class="video_thumbnail rounded-md shadow-md overflow-hidden md:max-w-xs">
-        <div class="flex flex-col">
-            <img class="object-cover" src="https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg" alt="Video Thumbnail">
+    <div ref="thumbnail"
+
+         :class="thumbnailClasses"
+         class="rounded-md shadow-md overflow-hidden hover:scale-125 transition duration-150 delay-0 motion-reduce:transition-none motion-reduce:hover:transform-none hover:duration-500 hover:delay-300"
+    >
+        <div class="flex flex-col min-h-[350px]">
+            <img :src="props.movie.Poster"
+                 alt="Video Thumbnail"
+                 class="object-cover">
         </div>
     </div>
 </template>
 
-<script>
-export default {
-    name: "VideoThumbnail"
+<script lang="ts" setup>
+const props = defineProps<{ movie: MovieSearch }>();
+const thumbnail = ref(null);
+const thumbnailClasses = ref({'origin-left': false, 'origin-right': false});
+const minSideDistanceToChangeOrigin = 60;
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize);
+  handleResize();
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize);
+})
+
+function handleResize() {
+  const thumbnailRect = thumbnail.value.getBoundingClientRect();
+  const screenWidth = window.innerWidth;
+  thumbnailClasses.value['origin-left'] = thumbnailRect.left <= minSideDistanceToChangeOrigin;
+  thumbnailClasses.value['origin-right'] = screenWidth - thumbnailRect.right <= minSideDistanceToChangeOrigin;
 }
 </script>

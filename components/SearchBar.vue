bc763de2ff9f2fb94ebf6468381cb6095c345c5b
Michael <michaeljrmoniz@gmail.com>
2023-04-17 10:46:49 -0300

    SearchBar mockup.

---
+++ components/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex flex-row w-full justify-center pb-8">
+        <input
+            type="text"
+            placeholder="Search for a movie by title"
+            class="w-full rounded-md border-1 border-gray-300 p-2"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SearchBar"
+}
+</script>

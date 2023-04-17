e191ab10a5900c2d539b8dc2439b2e9e1fdab89c
Michael <michaeljrmoniz@gmail.com>
2023-04-17 19:24:21 -0300

    Loading indicator.

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,15 +1,44 @@
 <template>
-    <div class="flex flex-row w-full justify-center pb-8">
+    <div class="flex flex-row w-full justify-center">
         <input
-            type="text"
-            placeholder="Search for a movie by title"
-            class="w-full rounded-md border-1 border-gray-300 p-2"
+                :value="searchQuery"
+                class="w-full border-1 border-gray-300 p-1 pl-2 rounded-md bg-amber-50 placeholder:italic placeholder:text-slate-400 text-sm"
+                placeholder="Search for a movie by title"
+                type="text"
+                @input="$emit('onNewSearchQuery', $event.target.value)"
         >
+        <template v-if="searchQuery">
+            <button class="ml-5 rounded-md w-12 bg-amber-50 flex justify-center items-center hover:scale-105"
+                    @click="decrementPage">
+                <Icon name="ic:baseline-arrow-back"></Icon>
+            </button>
+            <div class="ml-1 rounded-md w-12 bg-amber-50 flex justify-center items-center text-sm">
+                {{ currentPage }}
+            </div>
+            <button class="ml-1 rounded-md w-12 bg-amber-50 flex justify-center items-center hover:scale-105"
+                    @click="incrementPage">
+                <Icon name="ic:baseline-arrow-forward"></Icon>
+            </button>
+        </template>
     </div>
 </template>
 
-<script>
-export default {
-    name: "SearchBar"
+<script lang="ts" setup>
+const props = defineProps<{
+  searchQuery: string,
+  currentPage: number
+}>();
+const emit = defineEmits([
+  'onNewSearchQuery',
+  'onNewPage'
+]);
+
+function incrementPage() {
+  emit('onNewPage', props.currentPage + 1);
 }
+
+function decrementPage() {
+  emit('onNewPage', props.currentPage - 1);
+}
+
 </script>

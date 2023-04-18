d894382157e74a7e65fa1e310da2a31295b9b6be
Michael <michaeljrmoniz@gmail.com>
2023-04-18 19:35:45 -0300

    Add spacing between movie title and release date.

--- components/MovieDetails.vue
+++ components/MovieDetails.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="max-w-5xl bg-zinc-900/[.93] rounded-lg shadow-lg flex m-4 relative max-h-[70vh]" @click.stop>
-        <div :style="isMobile ? `background-image: url('${movieDetails.Poster != 'N/A'? movieDetails.Poster : useImageAsset('default-movie-poster.jpg')}')` : ''"
-             class="absolute bg-no-repeat bg-cover bottom-0 left-0 top-0 right-0 -z-10 blur-md">
-        </div>
-        <div class="absolute right-0 top-0 translate-x-1/2 -translate-y-1/2">
-            <button @click="$emit('close')">
-                <Icon name="ic:baseline-cancel" size="2em" style="color: white"></Icon>
-            </button>
-        </div>
         <template v-if="movieDetails">
+            <div :style="isMobile ? `background-image: url('${movieDetails.Poster != 'N/A'? movieDetails.Poster : useImageAsset('default-movie-poster.jpg')}')` : ''"
+                 class="absolute bg-no-repeat bg-cover bottom-0 left-0 top-0 right-0 -z-10 blur-md">
+            </div>
+            <div class="absolute right-0 top-0 translate-x-1/2 -translate-y-1/2">
+                <button @click="$emit('close')">
+                    <Icon name="ic:baseline-cancel" size="2em" style="color: white"></Icon>
+                </button>
+            </div>
             <div class="poster overflow-hidden rounded-l-md min-w-fit aspect-[2/3] hidden md:block z-50">
                 <img :alt="`${movieDetails.Title} Poster`"
                      :src="movieDetails.Poster != 'N/A'? movieDetails.Poster : useImageAsset('default-movie-poster.jpg')"
@@ -17,9 +17,8 @@
             </div>
             <div class="text-white p-6 flex flex-col w-full sm:bg-none overflow-auto">
                 <div class="space-y-2">
-                    <div
-                            class="text-2xl font-bold">{{ movieDetails.Title }}<span
-                            class="text-gray-300">({{ movieDetails.Released }})</span>
+                    <div class="text-2xl font-bold">{{ movieDetails.Title }}
+                        <span class="text-gray-300 ml-1">({{ movieDetails.Released }})</span>
                     </div>
                     <div>{{ movieDetails.Plot }}</div>
                 </div>

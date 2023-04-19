80dca8bc866f2b6fd5e9038a43deefd1cf9dad5e
Michael <michaeljrmoniz@gmail.com>
2023-04-18 21:32:46 -0300

    Refactoring.

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,29 +1,40 @@
 <template>
     <div class="flex flex-col sm:flex-row w-full justify-center space-y-2 sm:space-y-0 sm:space-x-2">
         <div class="flex flex-row justify-center space-x-2 w-full h-11">
-            <button v-if="searchQuery"
-                    class="rounded-md border border-zinc-700 min-w-fit w-16 flex justify-center items-center hover:scale-105 transition duration-150 bg-zinc-800 text-white"
-                    title="Reset search"
-                    @click="$emit('reset')"
-            >
-                <Icon name="ic:baseline-clear"></Icon>
-            </button>
-            <input
-                    :value="searchQuery"
-                    class="
-                    w-full border border-zinc-700 focus:border-zinc-200 focus:outline-none focus-visible:ring-0
-                    active:border-zinc-700 active:enabled:hover:border-zinc-700 p-1 pl-2 rounded-md bg-zinc-900
-                    placeholder:italic placeholder:text-zinc-200 text-base text-white"
-                    placeholder="Search for a movie by title"
-                    type="text"
-                    @input="$emit('newSearchQuery', $event.target.value)"
-            >
+            <label class="relative block w-full">
+                <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                    <Icon name="ic:baseline-search" size="1.2em" style="color: white"></Icon>
+                </span>
+                <input
+                        :value="searchQuery"
+                        aria-autocomplete="none"
+                        class="
+                            block w-full h-full border border-zinc-700 focus:border-zinc-200 focus:outline-none focus-visible:ring-0
+                            active:border-zinc-700 active:enabled:hover:autofill:border-zinc-700 p-2 pl-9 rounded-md bg-zinc-900
+                            placeholder:italic placeholder:text-zinc-200 text-base text-white
+                        "
+                        name="search"
+                        placeholder="Search for a movie by title"
+                        type="text"
+                        @input="$emit('newSearchQuery', $event.target.value)"
+                >
+                <span class="absolute inset-y-0 right-0 flex items-center pr-2 py-2">
+                     <button v-if="searchQuery"
+                             class="rounded-md h-full border border-zinc-700 min-w-fit w-10 flex justify-center items-center hover:scale-105 transition duration-150 bg-zinc-800 text-white"
+                             title="Reset search"
+                             @click="$emit('reset')"
+                     >
+                        <Icon name="ic:baseline-clear"></Icon>
+                    </button>
+                </span>
+            </label>
+
         </div>
         <div class="flex flex-row justify-center space-x-2 w-full h-11">
             <select
                     :class="{'min-w-full': !searchQuery}"
                     :value="yearOfRelease"
-                    class="form-select rounded-md border border-zinc-700 focus:border-zinc-200       focus:outline-none
+                    class="form-select rounded-md border border-zinc-700 focus:border-zinc-200 focus:outline-none
                     focus-visible:ring-0 active:border-zinc-700 active:enabled:hover:border-zinc-700 bg-zinc-800 text-white"
                     title="Filter by year of release"
                     @input="$emit('newYearOfRelease', $event.target.value)"
@@ -34,6 +45,7 @@
             <template v-if="searchQuery">
                 <button class="rounded-md border border-zinc-700 min-w-fit w-16 bg-zinc-800 flex justify-center items-center hover:scale-105 transition duration-150 text-white"
                         title="Go to previous page"
+                        type="button"
                         @click="$emit('newPage', currentPage - 1)"
                 >
                     <Icon name="ic:baseline-arrow-back"></Icon>
@@ -44,6 +56,7 @@
                 </div>
                 <button class="rounded-md border border-zinc-700 min-w-fit w-16 bg-zinc-800 flex justify-center items-center hover:scale-105 transition duration-150 text-white"
                         title="Go to next page"
+                        type="button"
                         @click="$emit('newPage', currentPage + 1)"
                 >
                     <Icon name="ic:baseline-arrow-forward"></Icon>

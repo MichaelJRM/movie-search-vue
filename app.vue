5a0126ff6ca62b23ea69f2ba6834b68b2e7914da
Michael <michaeljrmoniz@gmail.com>
2023-04-19 10:21:33 -0300

    Added Footer.

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template class="h-full">
     <div class="bg-zinc-900 min-h-screen">
         <NuxtPage/>
+        <Footer/>
     </div>
 </template>
 <script lang="ts" setup>
---
+++ components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="w-full p-4 grid place-items-center text-white">
+        <span>&copy; Michael Moniz {{ new Date().getFullYear() }}</span>
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
